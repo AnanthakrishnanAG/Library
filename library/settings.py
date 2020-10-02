8be8631f477995bb46bd7434ef881456b3548b11
MetaDevz <ananthakrishnam207@gmail.com>
2020-10-02 13:35:33 +0530

    first

--- library/settings.py
+++ library/settings.py
@@ -75,13 +75,33 @@ WSGI_APPLICATION = 'library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'Library Management',
+
+        'USER': 'hai',
+
+        'PASSWORD': 'root',
+
+        'HOST': 'http://127.0.0.1',
+
+        'PORT': '8001',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
